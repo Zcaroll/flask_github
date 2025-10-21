416f823639eed77ce889589806358566ba3f4d26
Zcaroll <qc0193ki@go.minneapolis.edu>
2025-10-21 15:36:13 -0500

    test

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
-
+from github_api import get_github_user
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # home page
 def homepage():
     return render_template('index.html')
+
+# handle requests to /get_user
+@app.route('/get_user') # dont forget the /
+def get_user_info():
+    # get user info and display on a new page
+    print('form data is', request.args)  # prints to terminal for debugging
+    username = request.args.get('username') # safer
+    # username = request.args['username'] # classic way to get form data. More error-prone
+    user_info, error_message = get_github_user(username) #user info dictionary
+    
+    # redirect user to an error page if there is an error
+    if error_message:
+        return render_template('error.html', error=error_message)
+    else:
+        return render_template('github.html', user_info=user_info) # redirects webpage to github.html
 
 
 if __name__ == '__main__':
--- github_api.py
+++ github_api.py
@@ -1,1 +1,34 @@
 # TODO make requests to github API
+
+import requests
+import logging
+
+def get_github_user(username):
+
+    # return a tuple of (data, error)
+    # if it works return (data, None)
+
+    # if it fails, or there is an error return (None, error)
+
+    try:
+        response = requests.get(f'https://api.github.com/users/{username}') # github user API
+        if response.status_code == 404: # not found. 
+            return None, f'Username {username} not found'
+        response.raise_for_status()  # throw an exception for 500 errors(where github's server isnt working)
+        response_json = response.json()
+        user_info = extract_user_info(response_json)
+        return user_info, None # return data, error
+    except Exception as e:
+        logging.exception(e)
+        return None, 'Error connecting to GitHub'
+
+# method for extracting useful info from github API response
+def extract_user_info(json_response):
+    # extract useful info from github_data dict
+    return {
+        'login': json_response.get('login'),
+        'name': json_response.get('name'),
+        'avatar_url': json_response.get('avatar_url'),
+        'home_page': json_response.get('html_url'),
+        'repos': json_response.get('public_repos'),
+    }
